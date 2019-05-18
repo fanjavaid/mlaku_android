90bc368016510b4ffe1ab77fe0104b3d551d041a
Fandi Akhmad <fanjavaid@gmail.com>
2019-05-19 00:31:09 +0700

    Remove unused code (#1)

--- app/src/main/java/com/mlaku/tourism/utils/Fragments.kt
+++
@@ -1,8 +0,0 @@
-package com.mlaku.tourism.utils
-
-/**
- * Created by Fandi Akhmad on 2019-05-05.
- */
-object Fragments {
-    const val FRAGMENT_STATE = "fragment_state"
-}
--- app/src/main/java/com/mlaku/tourism/utils/INavigationListener.kt
+++
@@ -1,8 +0,0 @@
-package com.mlaku.tourism.utils
-
-/**
- * Created by Fandi Akhmad on 2019-05-05.
- */
-interface INavigationListener {
-    fun onNextScreen(nextScreen: Int)
-}
